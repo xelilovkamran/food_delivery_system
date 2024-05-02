6f1c1b593b885fc1411a99f7d27ec3dfa0d88639
xelilovkamran <xlilovkamran@gmail.com>
2024-05-02 15:38:07 +0400

    first commit

---
+++ src/FoodItem.java
@@ -0,0 +1,17 @@
+class FoodItem {
+    private String name;
+    private double price;
+
+    public FoodItem(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
